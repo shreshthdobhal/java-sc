0fd6f3dd78182638349278a970a6227b53c86bce
shreshthdobhal <149085216+shreshthdobhal@users.noreply.github.com>
2024-07-15 05:27:15 +0000

    variable and const

---
+++ 01/variableandconst.js
@@ -0,0 +1,12 @@
+const accid=12345
+let accmail="dh@gmail.com"
+var accpass="gh"
+city="dun"
+let variable5;
+
+console.table([accid,accmail,accpass,city])
+
+accmail="kk@gmail.com"
+accpass="kk"
+
+console.table([accid,accmail,accpass,city,variable5])
