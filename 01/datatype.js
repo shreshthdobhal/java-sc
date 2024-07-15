9159594ecb2bf35144e8902341d95c9cedd7adf5
shreshthdobhal <149085216+shreshthdobhal@users.noreply.github.com>
2024-07-15 05:53:21 +0000

    changes

---
+++ 01/datatype.js
@@ -0,0 +1,17 @@
+"use strict";//newer version of js
+
+//alert(3+3)
+// does not work in nodejs onlly browser
+
+let ename="shreshth"
+let age=21
+let v1=null
+let v2;
+
+// datatype=> number, bigInt , string="" ,boolean= true/false
+// null=> standalone but when used in typeof the datatype is object
+// uundefined= undefined
+
+console.log(typeof "hello")
+console.log(typeof v1)
+console.log(typeof v2)
--- 01/variableandconst.js
+++ 01/variableandconst.js
@@ -7,7 +7,7 @@ let variable5;
 var keyword is used no more or very less because of it svope problem 
 const value can not be changed 
 let is used mostly
-any var with no value is undefined 
+any var with no value is undefined
 */
 
 
