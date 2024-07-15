8282b2ffd3fd5617bd103365f570142687ea656c
shreshthdobhal <149085216+shreshthdobhal@users.noreply.github.com>
2024-07-15 05:33:48 +0000

    asd

--- 01/variableandconst.js
+++ 01/variableandconst.js
@@ -3,6 +3,14 @@ let accmail="dh@gmail.com"
 var accpass="gh"
 city="dun"
 let variable5;
+/*
+var keyword is used no more or very less because of it svope problem 
+const value can not be changed 
+let is used mostly
+any var with no value is undefined 
+*/
+
+
 
 console.table([accid,accmail,accpass,city])
 
